27e6ede02d3dc1f8552684f913658b7fdbdd59d9
Marcio Tahyra <marciotahyra@gmail.com>
2025-02-24 09:25:26 -0300

    Exercicios 1,2 e 3 (Tipagem e arrays, funcao tipada e classe e instancia

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,24 @@
+// Introdução
 let mensagem: string = "Olá, TypeScript!";
 console.log(mensagem);
+
+// Exercício 1
+let valores: number[] = [10, 20, 30];
+let somaValores: number = valores.reduce((acc, val) => acc + val, 0);
+console.log("Soma dos valores:", somaValores);
+
+// Exercicío 2
+function mensagemPersonalizada(nome: string): string {
+    return `Olá, ${nome}! Bem-vindo ao TypeScript.`;
+}
+console.log(mensagemPersonalizada("Ana"));
+
+// Exercicío 3
+class Carro {
+    constructor(public marca: string, public modelo: string, public ano: number) {}
+    detalhes(): string {
+        return `${this.marca} ${this.modelo}, Ano: ${this.ano}`;
+    }
+}
+const carro1 = new Carro("Toyota", "Corolla", 2022);
+console.log(carro1.detalhes());
